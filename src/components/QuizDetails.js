326afb85fcb971c78d5013477e8cfb832eea2c5f
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-10-15 20:08:53 +0600

    full resposive

--- src/components/QuizDetails.js
+++ src/components/QuizDetails.js
@@ -9,16 +9,16 @@ const QuizDetails = ({ quizDetails }) => {
 
                 <div className='bg-gray-100 p-6 rounded shadow-lg'>
                     <img
-                        className='object-cover w-full h-56 mb-6 rounded shadow-lg sm:h-80 md:h-64 xl:h-80'
+                        className='object-cover w-full h-80 mb-6 rounded shadow-lg '
                         src={logo}
                         alt=''
                     />
-                    <p className='mb-2 text-xl font-bold leading-none sm:text-2xl'>{name}</p>
-                    <p className='text-gray-700 '>Total: {total}</p>
+                    {/* <p className='mb-2 text-xl font-bold leading-none sm:text-2xl'>{name}</p> */}
+                    <h1 className=' font-bold text-blue-600 text-center'>Question: {total}</h1>
                     <p
 
                     >
-                        <Link to={`/quiz/${id}`}><button className='px-8 block w-full mt-4 py-3 font-semibold rounded-full bg-cyan-200 text-gray-800 hover:bg-cyan-400'>{name}</button></Link>
+                        <Link to={`/quiz/${id}`}><button className='px-8 block w-full mt-4 py-3 font-semibold rounded-full bg-cyan-200 text-gray-800 hover:bg-cyan-400'>Start Quiz {name}</button></Link>
                     </p>
                 </div>
             </div>
