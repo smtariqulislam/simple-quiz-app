26882939640ca3ec698ee47b63562d8340adf954
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-10-15 11:19:13 +0600

    compontent create and header section complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+// import { RouterProvider } from 'react-router-dom'
+
+// import { router } from './Routes'
 
 function App() {
+  
   return (
-    <div className="App">
-      
+    <div>
+     <Header></Header>
+      <Home></Home>
     </div>
-  );
+    
+  )
+   
+     
+   
+  
 }
+export default App
 
-export default App;
+
---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quiz = () => {
+    return (
+        <div>
+            this is a quiz
+        </div>
+    );
+};
+
+export default Quiz;
