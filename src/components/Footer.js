52af5cedfe3daa0677c069dfd1760446891b1a26
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-10-15 20:43:41 +0600

    footer writting mistake

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="mt-20 px-4 divide-y bg-gray-800 text-gray-100">
-            <div className="py-6 text-sm text-center dark:text-gray-400">© 2022 S M Tariqul Islam create.</div>
+            <div className="py-6 text-sm text-center dark:text-gray-400">© 2022 By S M Tariqul Islam create.</div>
         </footer>
     );
 };
