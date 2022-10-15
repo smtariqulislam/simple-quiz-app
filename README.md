6e0fa45031a1150821ee25e23623eaaf426be5ad
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-10-15 21:13:49 +0600

    Add Readme.md file

---
+++ README.md
@@ -0,0 +1,19 @@
+# first npx create-react-app quiz-app
+
+## install react-dom 
+## install tailwine css
+## use third party component
+## create Blog file 
+## create Quiz file
+## create QuizDetail file
+## create Question file
+## create Answer file
+## route.js and root.js file create for routing and create nesting path
+## Make single-page applications with Link router. 
+## fetch data Routes.js to quiz.js and question.js file.
+## create Statics file.
+## Make it responsive.
+
+# My side:
+
+
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="mt-20 px-4 divide-y bg-gray-800 text-gray-100">
-            <div className="py-6 text-sm text-center dark:text-gray-400">© 2022 By S M Tariqul Islam create.</div>
-        </footer>
+        <body class="flex flex-col min-h-screen">
+            <footer className="mt-auto px-4 divide-y bg-gray-800 text-gray-100">
+                <div className="py-6 text-sm text-center dark:text-gray-400">© 2022 Create BY S M Tariqul Islam.</div>
+            </footer>
+        </body>
+        
     );
 };
 
